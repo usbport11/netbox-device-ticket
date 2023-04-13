aaffa439828c8af59257c3c65b1eb2bfbc73702b
usbport11 <51235513+usbport11@users.noreply.github.com>
2023-04-13 17:01:19 +0300

    Delete netbox_device_tickets directory

--- netbox_device_tickets/README.md
+++
@@ -1,3 +0,0 @@
-## netbox_device_tickets
-
-Manage device service tickets
--- netbox_device_tickets/netbox_device_tickets/api/urls.py
+++
@@ -1,9 +0,0 @@
-from netbox.api.routers import NetBoxRouter
-from . import views
-
-app_name = 'netbox_device_tickets'
-
-router = NetBoxRouter()
-router.register('device-tickets', views.DeviceTicketViewSet)
-
-urlpatterns = router.urls
